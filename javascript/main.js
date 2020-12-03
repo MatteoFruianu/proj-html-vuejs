d76767cbb16a61e110dc73ef43d47b61cd5f7a90
Matteo Fruianu <matteofruianu@gmail.com>
2020-12-03 15:24:37 +0100

    vue

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,35 @@ const app = new Vue ({
 
     data: {
 
-      
+      el: '#app',
+
+      crewDesc: 'THE DON PEPPE CREW FIRST AND FOREMOST VALUES AN AUTHENTIC, WELL BAKED SLICE OF PIZZA',
+
+      addresses: [
+          {
+              street: '1614 E.Bell Rd #104',
+              city: 'Salerno, AZ 85022',
+              mobile: '(602)867-1010'
+          },
+
+          {
+            street: '1614 Hemingway Rd #14',
+            city: 'Rome, za 85022',
+            mobile: '(602)867-1010'
+        },
+
+        {
+            street: '1614 Diana Rd #104',
+            city: 'London, XZ 85022',
+            mobile: '(602)867-1010'
+        },
+
+        {
+            street: '1614 Da Vinci Rd #104',
+            city: 'New York, PZ 85022',
+            mobile: '(602)867-1010'
+        },
+      ]  
 
     },
 
